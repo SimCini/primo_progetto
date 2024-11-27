83006dddda791734df886ff405133ff88d175582
SimCini <ciniltanisimone@gmail.com>
2024-11-27 11:41:46 +0100

    Passo 11

--- seconda_app/urls.py
+++ seconda_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from seconda_app.views import es_if, index
+from seconda_app.views import es_if, index, if_else_elif
 
 app_name = "seconda_app"
 urlpatterns = [
     path('es_if', es_if, name='es_if'),
+    path('if_else_elif', if_else_elif, name='if_else_elif'),
     path('', index, name='index'),
 ]
