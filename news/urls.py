c98e15119accef2bbbcc6c243e042bf1bbcf4e12
SimCini <ciniltanisimone@gmail.com>
2025-01-08 11:51:18 +0100

    passo17-18-19

--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, index, articoloDetailView, listaArticoli
+from .views import home, index, articoloDetailView, listaArticoli, queryBase, giornalistaDetailView
 
 app_name = "news"
 
@@ -8,4 +8,7 @@ urlpatterns = [
     path('home', home, name='homeview'),
     path('articoli/<int:pk>', articoloDetailView, name='articolo_detail'),
     path('articoli', listaArticoli, name='lista_articoli'),
+    path('giornalisti/<int:pk>', giornalistaDetailView, name='giornalista_detail'),
+    #path('giornalisti', listaGiornalisti, name='lista_giornalisti'),
+    path('query', queryBase, name='query_base')
 ]
