27931db8439c533b72cac4651df47edd0f5bdb77
SimCini <ciniltanisimone@gmail.com>
2025-04-01 14:38:39 +0200

    elimina-contatto

--- forms_app/urls.py
+++ forms_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from forms_app.views import contatti, lista_contatti, modifica_contatto
+from forms_app.views import contatti, lista_contatti, modifica_contatto, elimina_contatto
 
 app_name = "forms_app"
 urlpatterns = [
     path('contattacci/', contatti, name='contatti'),
     path('contatti/', lista_contatti, name='lista-contatti'),
     path('modifica_contatto/', modifica_contatto, name='modifica-contatto'),
+    path('elimina_contatto/', elimina_contatto, name='elimina-contatto'),
 ]
