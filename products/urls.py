a3f98384f6a9e938969468580ca0ee09df93815f
SimCini <ciniltanisimone@gmail.com>
2025-04-09 11:56:23 +0200

    passo 25 (prima parte)

---
+++ products/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from products.views import (product_detail, product_list, manufacturer_detail, manufacturer_list)
+
+app_name="products"
+
+urlpatterns = [
+    path('products/', product_list, name="product-list"),
+    path('products/<int:pk>/', product_detail, name="product_detail"),
+    path('manufacturers/', manufacturer_list, name="manufacterer-list"),
+    path('manufacturers/<int:pk>/', manufacturer_detail, name="manufacterer-detail"),
+]
